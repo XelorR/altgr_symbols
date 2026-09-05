0496d7d519b53d28f04aa42c567fbefcfbcafeb6
Peter <petr@duck.com>
2026-09-05 13:24:51 +0300

    windows ru layout built successfully

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ rm -rf dist
 kalamine build us.toml
 kalamine build ru.toml --out dist/ru-sym.xkb_keymap
 kalamine build ru.toml --out dist/ru-sym.xkb_symbols
+kalamine build ru.toml --qwerty-shortcuts --out dist/ru-sym.klc
