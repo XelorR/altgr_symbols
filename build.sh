117bfd642f51a4a8b71df654d4266e54a79acaa6
Peter <petr@duck.com>
2026-09-05 13:28:01 +0300

    rebuild all OSes with yo

--- build.sh
+++ build.sh
@@ -4,4 +4,7 @@ rm -rf dist
 kalamine build us.toml
 kalamine build ru.toml --out dist/ru-sym.xkb_keymap
 kalamine build ru.toml --out dist/ru-sym.xkb_symbols
+kalamine build ru.toml --out dist/ru-sym.keylayout
+kalamine build ru.toml --out dist/ru-sym.json
+kalamine build ru.toml --out dist/ru-sym.svg
 kalamine build ru.toml --qwerty-shortcuts --out dist/ru-sym.klc
